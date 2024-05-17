a8ce60784b78eb3667736bb73965f9ff61bccac9
ohranka <152566727+ohranka@users.noreply.github.com>
2024-05-17 18:17:34 +0300

    Practicum 7

--- practicum_7/homework/basic/inverse_power_method.py
+++ practicum_7/homework/basic/inverse_power_method.py
@@ -1,17 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from src.common import NDArrayFloat
-
 
 def inverse_power_method(A: NDArrayFloat, n_iters: int) -> NDArrayFloat:
+    x = np.random.rand(A.shape[0])
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    for _ in range(n_iters):
+        y = np.dot(A, x)
+        x = y / np.linalg.norm(y)
 
-    pass
+    eigenvalue = np.dot(x, np.dot(A, x))
 
+    return eigenvalue
 
 if __name__ == "__main__":
     A = np.array(
